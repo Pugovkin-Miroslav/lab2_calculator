34fcb7e064b05ef3c042b3a9b6b6ccb5070824b5
Miroslav <pugovkin_miroslav@mail.ru>
2025-09-16 09:20:48 +0300

    Попытка запустить тесты на github

--- include/rpn.h
+++ include/rpn.h
@@ -2,4 +2,14 @@
 #pragma once
 #include <stack>
 #include <string>
+
+/**
+ * @brief EvaLuates a mathematical expression in Reserve Polish Notation (RPN).
+ *
+ * @param expression A string containing the RPN expression (e.g., "2 3 +").
+ * Tokens must be separated by spaces
+ * @return double The result of evaluated expression.
+ * @exception std::invalid_argument Thrown if the exspression is invalid,
+ * contains on unknown operator, or attempts division by zero.
+ */
 double evaluate_rpn(const std::string& expression);
